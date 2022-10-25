d6e6e222ecdaae82de86ddec0b378a1a3de9256b
Ano-acc <114471913+Ano-acc@users.noreply.github.com>
2022-10-25 15:23:53 +0200

    [EVOLV]Main

--- dailyban/lib/main.dart
+++ dailyban/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:dailyban/widget/HomePage/home_page_view.dart';
+import 'package:dailyban/widget/TeamPage/team_page_bindings.dart';
+import 'package:dailyban/widget/UserPage/user_page_bindings.dart';
+import 'package:dailyban/widget/UserPage/user_page_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import 'widget/TeamPage/team_page_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,6 +25,16 @@ class MyApp extends StatelessWidget {
         GetPage(
           name: "/home",
           page: () => const HomePage(),
+        ),
+        GetPage(
+          name: "/summoner",
+          page: () => const UserPageView(),
+          binding: UserPageBindings(),
+        ),
+        GetPage(
+          name: "/team",
+          page: () => const TeamPageView(),
+          binding: TeamPageBindings(),
         )
       ],
       initialRoute: "/home",

501e734fd96f4fcaac3f99d766a19eb87b1c2223
Ano-acc <114471913+Ano-acc@users.noreply.github.com>
2022-10-25 15:24:14 +0200

    [EVOLV]Pages

---
+++ dailyban/lib/widget/UserPage/user_page_view.dart
@@ -0,0 +1,12 @@
+import 'package:dailyban/widget/UserPage/user_page_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class UserPageView extends GetView<UserPageController> {
+  const UserPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

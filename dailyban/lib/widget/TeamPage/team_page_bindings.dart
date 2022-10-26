033ce9d124c97c346291932b6eb15d0ab8d71e5f
Ano-acc <114471913+Ano-acc@users.noreply.github.com>
2022-10-26 16:08:05 +0200

    [FIX] Delete widget

--- dailyban/lib/widget/TeamPage/team_page_bindings.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dailyban/widget/TeamPage/team_page_controller.dart';
-import 'package:get/get.dart';
-
-class TeamPageBindings extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => TeamPageController());
-  }
-}
